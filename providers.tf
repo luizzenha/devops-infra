00d63a4a74da267a5cfa7dc4e4eee1ddc6580850
Luiz Zenha <lzenha@gmail.com>
2021-11-06 09:39:16 -0300

    Terraform Cloud

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  credentials = file("/Users/luizzenha/Desenv/MBA/lab_cloud/VMsVagrant/gcpkey.json")
-
   project = "zenha-329914"
   region  = "us-west1"
   zone    = "us-west1-b"
